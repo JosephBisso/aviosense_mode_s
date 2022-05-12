8b100c1557bf04471cc47a91ba83aaac991899f6
Joseph Bisso-Bi-Ela <j.bisso-bi-ela@tu-braunschweig.de>
2022-05-12 23:54:43 +0200

    used relative paths

--- run.ps1
+++ run.ps1
@@ -3,12 +3,12 @@
 $ErrorActionPreference = 'Stop'
 
 Write-Host ("# Building Ressources...") -ForegroundColor Cyan
-. pyside6-rcc "D:\BA\ba_mode-s_analysis\qml\qml.qrc" -o "D:\BA\ba_mode-s_analysis\qml\qrc_qml.py" 
+. pyside6-rcc  "$(Get-ChildItem .\qml\qml.qrc)" -o "$(Get-ChildItem .\qml\qml.qrc)\qrc_qml.py" 
 
 $? ? (Write-Host ("->Done.") -ForegroundColor Green) : (Write-Error ("App finished unnormay.. Last exit code: $LASTEXITCODE")) 
 
 Write-Host ("# Starting the app...") -ForegroundColor Cyan
-$app = Start-Process -FilePath "python" -ArgumentList "D:\BA\ba_mode-s_analysis\mode_s\main.py", "$args" -WorkingDirectory "D:\BA\ba_mode-s_analysis" -NoNewWindow -PassThru -Wait
+$app = Start-Process -FilePath "python" -ArgumentList "$(Get-ChildItem .\mode_s\main.py)", "$args" -WorkingDirectory "$(Get-Location)" -NoNewWindow -PassThru -Wait
 
 $? ? (Write-Host ("App existed normally.") -ForegroundColor Green) : (Write-Error ("App finished unnormay.. Last exit code: $LASTEXITCODE")) 
 exit $LASTEXITCODE
