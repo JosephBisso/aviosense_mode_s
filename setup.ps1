6ae39faeb16e47965ed7633ffb51b6c9bdb2f6c1
JosephBisso <64443075+JosephBisso@users.noreply.github.com>
2022-11-20 22:06:55 +0100

    Also checking mysql dlls in script.ps1 and adjusting actions

--- setup.ps1
+++ setup.ps1
@@ -71,10 +71,37 @@ if (-not $mysql) {
     Write-Host ">> MySql must be installed AND added to PATH" -ForegroundColor Red
     Write-Host "[ ] Download mysql here 'https://dev.mysql.com/downloads/mysql/'" -ForegroundColor DarkYellow
     Write-Host "[ ] Or install it with choco: 'choco install -y mysql'" -ForegroundColor DarkYellow
-    Write-Host "If already installed, add it to the PATH" -ForegroundColor DarkYellow
+    Write-Host "|-> If already installed, add it to the PATH" -ForegroundColor DarkYellow
     exit -1
 } else {
-    Write-Host ">> Done" -ForegroundColor Green
+    $mysql_dll = Get-Command libmysql.dll -ErrorAction SilentlyContinue
+    if (-not $mysql_dll) {
+        $mysql_dir = (Get-Item $mysql.Source).Directory.Parent
+        $mysql_dll_dir = "$mysql_dir/lib"
+        if (Test-Path $mysql_dll_dir) {
+            try {
+                [Environment]::SetEnvironmentVariable(
+                "Path",
+                "$mysql_dll_dir;" + [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine),
+                [EnvironmentVariableTarget]::Machine)
+
+                Write-Host ">> Done" -ForegroundColor Green
+
+            } catch {
+                Write-Host ">> Could not add $mysql_dll_dir to the PATH." -ForegroundColor Red
+                Write-Host $_ -ForegroundColor DarkYellow
+                Write-Host "|-> Missing Admin Rights. Please run this script as ADMIN or manually add $mysql_dll_dir to the PATH" -ForegroundColor DarkYellow
+                exit -1
+            }
+        } else {
+            Write-Host ">> Cannot find $mysql_dll_dir." -ForegroundColor Red
+            Write-Host "|-> Where is libmysql.dll ?? Please also add the location to the PATH" -ForegroundColor DarkYellow
+            exit -1
+        }
+    } else {
+        Write-Host ">> Done" -ForegroundColor Green
+    }
+    
 }
 
 Write-Host "`n`nSUCCESS: Setup completed" -ForegroundColor Green
